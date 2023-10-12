be2075b1fcd603e75bf61087da6ef198ff032f2a
kbkbowo <43379407+kbkbowo@users.noreply.github.com>
2023-10-12 20:57:06 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,11 +1,18 @@
-
-
-if $1 == "metaworld"; then
-    wget https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/metaworld/model-24.pt -o results/mw/model-24.pt
-elif $1 == "ithor"; then
-    wget https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/ithor/model-30.pt -o results/thor/model-30.pt
-elif $1 == "bridge"; then
-    wget https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/bridge/model-42.pt -o results/bridge/model-42.pt
+if [ "$1" = "metaworld" ]; then
+    wget -c https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/metaworld/model-24.pt
+    mkdir -p results/mw
+    mv model-24.pt results/mw/model-24.pt
+    echo "Downloaded metaworld model"
+elif [ "$1" = "ithor" ]; then
+    wget -c https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/ithor/model-30.pt
+    mkdir -p results/thor
+    mv model-30.pt results/thor/model-30.pt
+    echo "Downloaded ithor model"
+elif [ "$1" = "bridge" ]; then
+    wget -c https://huggingface.co/Po-Chen/flowdiffusion/resolve/main/ckpts/bridge/model-42.pt
+    mkdir -p results/bridge
+    mv model-42.pt results/bridge/model-42.pt
+    echo "Downloaded bridge model"
 else 
     echo "Options: {metaworld, ithor, bridge}"
 fi
